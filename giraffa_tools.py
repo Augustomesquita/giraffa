1ab20a14c66293ed3ea61e218f1111c2c8effc61
Augustomesquita <augustomesquitasrs@gmail.com>
2020-05-28 22:41:23 -0300

    First commit

---
+++ giraffa_tools.py
@@ -0,0 +1,57 @@
+def get_data_with_pagination():
+    data = []
+    limit_result = 100
+    for offset in range(0, 1000, limit_result):
+        data.append(offset)
+    return data
+
+
+def get_character():
+    character = {
+        "life": 500,
+        "mana": 200,
+        "equipaments": {
+            "helmet": "Royal Helmet",
+            "armor": "Demom Armor"
+        }
+    }
+
+    character.update({"life": 1500})
+    character["life"] = 2000
+    return character
+
+
+def get_input_number():
+    try:
+        return int(input("Enter a number: "))
+    except ValueError:
+        print("Invalid input number. Try again.")
+        return get_input_number()
+
+
+def input_file_content(file_name):
+    text_file = open(file_name, "w")
+    text_content = [
+        prepare_text("this is my sample"),
+        prepare_text("of text file"),
+        prepare_text("in python."),
+        prepare_text("my mission here is open,"),
+        prepare_text("write and read this thing."),
+    ]
+    text_file.writelines(text_content)
+    text_file.close()
+
+
+def prepare_text(text):
+    return "\n" + text
+
+
+def output_file_content(file_name):
+    text_file = open(file_name, "r")
+    print(text_file.read())
+    text_file.seek(0)
+
+    for text in text_file.readlines():
+        print(text)
+
+    text_file.close()
